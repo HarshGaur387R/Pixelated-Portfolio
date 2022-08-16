45b291d14a21a86e85d5039ff8722fc1f3133937
HarshGa387R <hgaur701@gmail.com>
2022-08-16 19:55:10 +0530

    making_animated_meters

--- script.js
+++ script.js
@@ -1,12 +1,109 @@
+// const y = document.getElementById("meters");
+// if (y) {
+//     y.addEventListener("mouseover", state);
+// }
+// else {
+//     console.log("element (html) is returning null.");
+// }
+
+var times = 0;
+var id;
+
 function closeSidebar() {
     document.getElementById("sidebar").style.translate = "-200px";
     document.getElementById("cross").style.transform = "translate(-200px)";
     document.getElementById("hamburger").style.transform = "translate(0px)";
-    
+
 }
 
-function openSidebar(){
-     document.getElementById("sidebar").style.translate = "0px";
-     document.getElementById("cross").style.transform = "translate(0px)";
+function openSidebar() {
+    document.getElementById("sidebar").style.translate = "0px";
+    document.getElementById("cross").style.transform = "translate(0px)";
     document.getElementById("hamburger").style.transform = "translate(-200px)";
 }
+
+function state() {
+    // y.removeEventListener("mouseover", state);
+    // setTimeout(function () {
+    // y.addEventListener("mouseover", state);
+    // }, 5000);
+
+    // window.removeEventListener('scroll', state);
+
+    // setTimeout(function () {
+    //     window.addEventListener("scroll",state)
+    // }, 5000);
+
+    var screen = document.getElementById("CSStext");
+    if (isInViewPort(screen) && times == 0) {
+        times = 1;
+        move();
+    }
+    else if(isInViewPort(screen) && times == 1){
+        clearInterval(id);
+        move();
+        
+    }
+}
+
+function move() {
+
+    let r = document.querySelector(':root');
+    let rs = getComputedStyle(r);
+    
+    let HTMLelement = document.getElementById("HTMLtext");
+    let CSSelement = document.getElementById("CSStext");
+    let JSelement = document.getElementById("JStext");
+
+    let counter = 0;
+    let width = 0;
+
+    id = setInterval(frame, 20);
+    
+    function frame() {
+        if (width == 100) {
+            clearInterval(id);
+        }
+        else {
+            width++;
+            counter++;
+
+            if(width<=95){
+            HTMLelement.innerText = counter + '%';
+            r.style.setProperty("--hpos", width + '%');
+            }
+            if(width<=80){
+            CSSelement.innerText = counter +'%';
+            r.style.setProperty("--cpos",width+'%');
+            }
+            if(width<=90){
+            JSelement.innerText = counter + '%';
+            r.style.setProperty("--jpos",width+'%');
+            }
+        }
+
+    }
+
+}
+
+function isInViewPort(element) {
+    // Get the bounding client rectangle position in the viewport
+    var bounding = element.getBoundingClientRect();
+
+    // Checking part. Here the code checks if it's *fully* visible
+    // Edit this part if you just want a partial visibility
+    if (
+        bounding.top >= 0 &&
+        bounding.left >= 0 &&
+        bounding.right <= (window.innerWidth || document.documentElement.clientWidth) &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    ) {
+        console.log('In the viewport! :)');
+        return true;
+    } else {
+        console.log('Not in the viewport. :(');
+        return false;
+    }
+}
+
+window.addEventListener('scroll', state, false);
