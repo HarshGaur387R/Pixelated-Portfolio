e0fa6ff322de97e8597c25b62af4ff792b1c3f75
HarshGa387R <hgaur701@gmail.com>
2022-08-23 14:38:24 +0530

    making some changes

--- script.js
+++ script.js
@@ -10,14 +10,14 @@ var times = 0;
 var id;
 
 function closeSidebar() {
-    document.getElementById("sidebar").style.translate = "-200px";
+    document.getElementById("sidebar").style.transform = "translate(-200px)";
     document.getElementById("cross").style.transform = "translate(-200px)";
     document.getElementById("hamburger").style.transform = "translate(0px)";
 
 }
 
 function openSidebar() {
-    document.getElementById("sidebar").style.translate = "0px";
+    document.getElementById("sidebar").style.transform = "translate(0px)";
     document.getElementById("cross").style.transform = "translate(0px)";
     document.getElementById("hamburger").style.transform = "translate(-200px)";
 }
